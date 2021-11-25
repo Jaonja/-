792edda5b8803a110f23662eaf14012bc1ace195
yaros <golishon0@gmail.com>
2021-11-25 14:44:13 +0300

    Update ChekboxWrapper.spec.js

--- vue-movie/tests/unit/components/organism/ChekboxWrapper.spec.js
+++ vue-movie/tests/unit/components/organism/ChekboxWrapper.spec.js
@@ -12,7 +12,7 @@ describe("ChekboxWrapper", () => {
 
   beforeEach(() => {
     getters = {
-      showSortBtn: () => [
+      taskState: () => [
         {
           id: 1,
           text: "All",
