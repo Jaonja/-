2f73ecba4edd89b871a0ef9a354e9d16e36bce18
yaros <golishon0@gmail.com>
2021-11-25 14:46:59 +0300

    fix

--- vue-movie/tests/unit/components/atoms/ChekboxTask.spec.js
+++ vue-movie/tests/unit/components/atoms/ChekboxTask.spec.js
@@ -2,36 +2,33 @@ import Vuex from "Vuex";
 import { mount, createLocalVue, shallowMount } from "@vue/test-utils";
 import ChekboxTask from "@/components/atoms/ChekboxTask.vue";
 
+const localVue = createLocalVue();
 
+localVue.use(Vuex);
 
-
-const localVue = createLocalVue()
-
-localVue.use(Vuex)
-
-describe('ChekboxTask', () => {
-  let mutations
-  let store
+describe("ChekboxTask", () => {
+  let mutations;
+  let store;
 
   beforeEach(() => {
     mutations = {
       delTask: jest.fn(),
-			CheckBoxNext: jest.fn(),
-    }
+      CheckBoxNext: jest.fn(),
+    };
     store = new Vuex.Store({
-      mutations
-    })
-  })
+      mutations,
+    });
+  });
 
-	test("", () => {
-		let wrapper = mount(ChekboxTask, { localVue });
-		expect(wrapper.vm).toBeTruthy();
-		expect(wrapper.is(ChekboxTask)).toBeTruthy();
+  test("", () => {
+    let wrapper = mount(ChekboxTask, { localVue });
+    expect(wrapper.vm).toBeTruthy();
+    expect(wrapper.is(ChekboxTask)).toBeTruthy();
+  });
+
+  test("", () => {
+    let wrapper = mount(ChekboxTask, { store, localVue });
+    wrapper.findAll("div").at(1).trigger("click");
+    expect(mutations.delTask).toHaveBeenCalled();
+  });
 });
-
-		test("", () => {
-			let wrapper = mount(ChekboxTask, { store, localVue });
-			wrapper.findAll("div").at(1).trigger("click");
-			expect(mutations.delTask).toHaveBeenCalled();
-		});
-	});
